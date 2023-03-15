2b28305ee0a8daa094dc243237811ec49d7b992b
DonovanZeanah <donovan.zeanah@outlook.com>
2023-03-15 14:48:23 -0500

    organized

---
+++ WorkshopGroup/ClientApp/KingdomAngular/src/app/models/skill.ts
@@ -0,0 +1,14 @@
+export class Skill {
+
+    id?: number;
+    name = "";
+    level = 0;
+    tools = { items : "", count : 0 };
+    description = "";
+    function = "";
+    subjectField = "";
+
+
+
+
+}
