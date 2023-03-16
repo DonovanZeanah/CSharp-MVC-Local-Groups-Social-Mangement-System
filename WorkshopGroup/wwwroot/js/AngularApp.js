ab633c1fd670cd57ade4c0c646660448410b6071
DonovanZeanah <donovan.zeanah@outlook.com>
2023-03-16 17:50:29 -0500

    "angular Js Implimentation ontop of MVC architecture"

---
+++ WorkshopGroup/wwwroot/js/AngularApp.js
@@ -0,0 +1,6 @@
+var app = angular.module('myAngularApp', []);
+
+app.controller('myController', ['$scope', function ($scope) {
+    alert('Hello from AngularJS');
+    // Your controller logic here
+}]);
